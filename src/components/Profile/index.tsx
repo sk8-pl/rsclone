e2275723ceb54808a4e085551c59ce8220991cb5
Anna <missjellx@gmail.com>
2022-02-02 20:49:59 +0300

    feat: add profile page

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,5 +1,62 @@
 import "./style.css";
 
-const Profile = () => <div className="profile">Profile</div>;
+const Profile = () => (
+  <div className="profile">
+    <div className="container">
+      <div className="profile-info">
+        <div className="avatar-img"></div>
+        <div className="user-info">
+          <div className="greeting">Здравствуйте, User! Рады видеть вас!</div>
+          <div className="wrapper-info">
+            <span className="info-title">Редактировать информацию о себе:</span>
+            <div className="info-block">
+              <span className="info-name">Имя</span>
+              <input type="text" className="info-input" />
+            </div>
+            <div className="info-block">
+              <span className="info-name">Фамилия</span>
+              <input type="text" className="info-input" />
+            </div>
+            <div className="info-block">
+              <span className="info-name">E-mail</span>
+              <input type="text" className="info-input" />
+            </div>
+            <div className="info-block">
+              <span className="info-name">Телефон</span>
+              <input type="text" className="info-input" />
+            </div>
+          </div>
+          <div className="booking-info">
+            <span className="info-title">Ваши бронирования:</span>
+            <div className="booking-item">
+              <div className="booking-img"></div>
+              <div className="booking-hotel-info">
+                <span className="hotel-info hotel-info-name">
+                  Hotel Park Inn
+                </span>
+                <span className="hotel-info">20 дек. 2018 - 13 янв. 2019</span>
+              </div>
+              <span className="hotel-info-name">
+                <b>14 000</b>
+              </span>
+            </div>
+            <div className="booking-item">
+              <div className="booking-img"></div>
+              <div className="booking-hotel-info">
+                <span className="hotel-info hotel-info-name">
+                  Hotel Park Inn
+                </span>
+                <span className="hotel-info">20 дек. 2018 - 13 янв. 2019</span>
+              </div>
+              <span className="hotel-info-name">
+                <b>14 000</b>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Profile;
