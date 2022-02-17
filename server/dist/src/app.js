455b36402e8ddfb09524dff8db903c625f5fc774
Olga Provedentceva <77030402+lost-fox@users.noreply.github.com>
2022-02-17 23:29:30 +0300

    feat: getting random cities from database (#29)
    * feat: getting random cities from database
    
    * fix: delete post and add 404

--- server/dist/src/app.js
+++ server/dist/src/app.js
@@ -18,6 +18,7 @@ const config_1 = __importDefault(require("config"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const auth_routes_1 = __importDefault(require("../routes/auth.routes"));
 const user_roures_1 = __importDefault(require("../routes/user.roures"));
+const cities_roures_1 = __importDefault(require("../routes/cities.roures"));
 const morgan_1 = __importDefault(require("morgan"));
 const app = (0, express_1.default)();
 const PORT = config_1.default.get("port") || 8080;
@@ -25,6 +26,7 @@ app.use((0, cors_1.default)());
 app.use((0, morgan_1.default)("tiny"));
 app.use(express_1.default.json());
 app.use("/user", user_roures_1.default);
+app.use("/cities", cities_roures_1.default);
 app.use("/", auth_routes_1.default);
 function start() {
     return __awaiter(this, void 0, void 0, function* () {
